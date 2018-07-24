6305e7e869990845f8f0e4c4cac10858487b11e4
Lucas Dev <lucas_ss.arts@live.com>
2018-07-24 12:26:48 -0300

    Up project

---
+++ js/chrome.js
@@ -0,0 +1,5 @@
+$('input[name="btnK"]').before('<input id="chExtension" value="Extension Google" aria-label="Extension Google" name="btnX" type="submit" jsaction="sf.chk">');
+
+$("input[name='btnK']").click(function() {
+    alert("aqui");
+});
